122c7e9f0f71b77d0049f8aa69a8dd1a60676da0
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-08-16 22:52:36 -0600

    attempt 2 at background

--- client/src/index.js
+++ client/src/index.js
@@ -7,24 +7,24 @@ import './assets/stylesheets/style.css';
 // import background from '../public/cover.png';
 
 // 1. import `ChakraProvider` component
-import { extendTheme, ChakraProvider } from '@chakra-ui/react';
+// import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 
-const colors = {
-  brand: {
-    1000: '9ca1a5',
-    900: '#2ecOea',
-    800: '#1289c8',
-    700: '4d3e3f',
-    600: '2d4d7a',
-    500: '889ba2',
-    400: 'd2d2d6',
-    300: '96654b',
-    200: '206c87',
-    100: 'c98d71',
-  },
-};
+// const colors = {
+//   brand: {
+//     1000: '9ca1a5',
+//     900: '#2ecOea',
+//     800: '#1289c8',
+//     700: '4d3e3f',
+//     600: '2d4d7a',
+//     500: '889ba2',
+//     400: 'd2d2d6',
+//     300: '96654b',
+//     200: '206c87',
+//     100: 'c98d71',
+//   },
+// };
 
-const theme = extendTheme({ colors });
+// const theme = extendTheme({ colors });
 
 // function ChakraApp() {
 //   // 2. Wrap ChakraProvider at the root of your app
@@ -34,18 +34,18 @@ const theme = extendTheme({ colors });
 //     </ChakraProvider>
 //   );
 // }
-function ImageApp (){
-  return(
-    <div style={{ backgroundImage: "url(/cover.png"}} >
-      testing
-    </div>
-  );
-}
+// function ImageApp (){
+//   return(
+//     <div style={{ backgroundImage: "url(/cover.png"}} >
+//       testing
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ImageApp />
+    <App />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
